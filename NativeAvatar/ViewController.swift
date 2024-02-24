f286ac92a9adf0bc251fc8f0672e2dc06a10e675
temasarkisov <a.sarkisov.s@yandex.ru>
2024-02-24 16:47:11 +0200

    Initial Commit

---
+++ NativeAvatar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NativeAvatar
+//
+//  Created by Артем Саркисов on 24.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
